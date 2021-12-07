a88f313849d2d4de45718303b3d6303f869477b5
SeungMinChoi <ccm1333@naver.com>
2021-12-07 17:48:54 +0900

    navigation

---
+++ Navigation/Navigation/EditViewController.swift
@@ -0,0 +1,75 @@
+//
+//  EditViewController.swift
+//  Navigation
+//
+//  Created by 최승민 on 2021/12/07.
+//
+
+import UIKit
+
+protocol EditDelegate {
+    func didMessageEditDone(_ controller: EditViewController, message: String)
+    func didImageOnOffDone(_ controller: EditViewController, isOn: Bool)
+    func didImageZoomDone(_ controller: EditViewController, isZoom: Bool)
+}
+
+class EditViewController: UIViewController {
+    var textWayValue: String = ""
+    var textMessage: String = ""
+    var delegate: EditDelegate?
+    var isOn = false
+    var isZoom = false
+    
+    @IBOutlet var lblWay: UILabel!
+    @IBOutlet var txMessage: UITextField!
+    @IBOutlet var swIsOn: UISwitch!
+    @IBOutlet var btnZoom: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        lblWay.text = textWayValue
+        txMessage.text = textMessage
+        swIsOn.isOn = isOn
+    }
+    
+    @IBAction func swIsOn(_ sender: Any) {
+        
+    }
+    
+    @IBAction func btnDone(_ sender: UIButton) {
+        if delegate != nil {
+            delegate?.didMessageEditDone(self, message: txMessage.text!)
+            delegate?.didImageOnOffDone(self, isOn: isOn)
+            delegate?.didImageZoomDone(self, isZoom: isZoom)
+        }
+        _ = navigationController?.popViewController(animated: true)
+    }
+    
+    @IBAction func swImageOnOff(_ sender: UISwitch) {
+        if sender.isOn {
+            isOn = true
+        } else {
+            isOn = false
+        }
+    }
+    
+    @IBAction func btnZoom(_ sender: UIButton) {
+        if isZoom {
+            isZoom = false
+        } else {
+            isZoom = true
+        }
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Navigation/Navigation/ViewController.swift
@@ -0,0 +1,65 @@
+//
+//  ViewController.swift
+//  Navigation
+//
+//  Created by 최승민 on 2021/12/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController, EditDelegate {
+    let imgOn = UIImage(named: "lamp_on.png")
+    let imgOff = UIImage(named: "lamp_off.png")
+    
+    var isOn = true
+    var isZoom = false
+    
+    @IBOutlet var txMessage: UITextField!
+    @IBOutlet var imgView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        imgView.image = imgOn
+    }
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+        let editViewController = segue.destination as! EditViewController
+        if segue.identifier == "editButton" {
+            editViewController.textWayValue = "segue : use button"
+        } else if segue.identifier == "editBarButton" {
+            editViewController.textWayValue = "segue : use Bar button"
+        }
+        editViewController.delegate = self
+        editViewController.textMessage = txMessage.text!
+        editViewController.isOn = isOn
+        editViewController.isZoom = isZoom
+    }
+    
+    func didMessageEditDone(_ controller: EditViewController, message: String) {
+        txMessage.text = message
+    }
+    
+    func didImageOnOffDone(_ controller: EditViewController, isOn: Bool) {
+        if isOn {
+            imgView.image = imgOn
+            self.isOn = true
+        } else {
+            imgView.image = imgOff
+            self.isOn = false
+        }
+    }
+    
+    func didImageZoomDone(_ controller: EditViewController, isZoom: Bool) {
+        if isZoom {
+            self.isZoom = isZoom
+            imgView.frame.size = CGSize(width: 200, height: 200)
+        } else {
+            self.isZoom = isZoom
+            imgView.frame.size = CGSize(width: 100, height: 100)
+        }
+    }
+}
+
