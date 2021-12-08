c03fd077725fe814e13200aa5d682e6813eb7bf1
SeungMinChoi <ccm1333@naver.com>
2021-12-08 09:40:39 +0900

    auto layout

---
+++ AutoLayout/AutoLayout/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AutoLayout
+//
+//  Created by 최승민 on 2021/12/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
